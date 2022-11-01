1080b347403d6b934aa2f41f40f13497e1d214c3
Sam Ginzburg <ginzburg.sam@gmail.com>
2022-11-01 14:43:06 -0400

    updates

--- install.sh
+++ install.sh
@@ -1,3 +1,21 @@
 mkdir -p ~/.config/nvim/
 cp init.vim ~/.config/nvim/init.vim
 cp init.vim ~/.vimrc
+
+# install vim-plug
+# nvim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+# vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# install fonts
+git clone --filter=blob:none --sparse git@github.com:ryanoasis/nerd-fonts
+cd nerd-fonts
+git sparse-checkout add patched-fonts/JetBrainsMono
+./install.sh JetBrainsMono
+cd ..
+
+# set terminal font
+
